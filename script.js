59d6e53a4f07d55e98e57ea8b8339a1c57654c7a
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2023-09-13 09:15:13 +0200

    Buttons.

--- script.js
+++ script.js
@@ -13,25 +13,45 @@ sections.forEach(function(section) {
   });
 });
 
-const title = document.getElementById("section_ourservices_title_h3")
+const title = document.getElementById("section_ourservices_title_h3");
 const description = document.getElementById('service-description');
+container1 = document.getElementById("section_ourservices_container_1");
+container2 = document.getElementById("section_ourservices_container_2");
+container3 = document.getElementById("section_ourservices_container_3");
+container4 = document.getElementById("section_ourservices_container_4");
 
 document.getElementById('section_ourservices_button1').onclick = function() {
     title.textContent = "E-Commerce"
     description.textContent = "E-Commerce";
+    container1.style.backgroundColor = "rgb(56, 27, 117)";
+    container2.style.backgroundColor = "black";
+    container3.style.backgroundColor = "black";
+    container4.style.backgroundColor = "black";
 };
 
 document.getElementById('section_ourservices_button2').onclick = function() {
   title.textContent = "Agencies"
   description.textContent = "Agencies";
+  container1.style.backgroundColor = "black";
+  container2.style.backgroundColor = "rgb(56, 27, 117)";
+  container3.style.backgroundColor = "black";
+  container4.style.backgroundColor = "black";
 };
 
 document.getElementById('section_ourservices_button3').onclick = function() {
   title.textContent = "Real Estate"
   description.textContent = "Real Estate";
+  container1.style.backgroundColor = "black";
+  container2.style.backgroundColor = "black";
+  container3.style.backgroundColor = "rgb(56, 27, 117)";
+  container4.style.backgroundColor = "black";
 };
 
 document.getElementById('section_ourservices_button4').onclick = function() {
   title.textContent = "Consultants"
   description.textContent = "Consultants";
+  container1.style.backgroundColor = "black";
+  container2.style.backgroundColor = "black";
+  container3.style.backgroundColor = "black";
+  container4.style.backgroundColor = "rgb(56, 27, 117)";
 };
