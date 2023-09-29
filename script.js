20b12b8433c71d3d26457c0afedc25335abddd8a
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2023-09-29 19:48:30 +0200

    Responsive design.

--- script.js
+++ script.js
@@ -153,3 +153,19 @@ function changeSlide(n) {
 }
 
 showSlide(currentSlide);
+
+var sections = document.querySelectorAll(".section_smallerservices_dropdown_container");
+sections.forEach(function(section) {
+  var button = section.querySelector(".section_smallerservices_dropdown_button");
+  var content = section.querySelector(".section_smallerservices_dropdown_collapsible_container");
+  var icon1 = section.querySelector(".section_smallerservices_dropdown_icon");
+  var icon2 = section.querySelector(".section_smallerservices_dropdown_collapsible_icon");
+
+  button.addEventListener("click", function() {
+    section.classList.toggle("active");
+    content.style.display = (content.style.display === "block") ? "none" : "block";
+    icon1.style.display = (content.style.display === "block") ? "none" : "block";
+    icon2.style.display = (content.style.display === "block") ? "block" : "none";
+  });
+});
+
